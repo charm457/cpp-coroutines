b3824c91d075801e54c2f276bf6ad5c103c32ce7
charm457 <byzovdima34@gmail.com>
2026-05-03 10:48:15 +0300

    Increase mario and  use ncurses

--- SuperMario/mainCopyWrite.c
+++ SuperMario/mainCopyWrite.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ncurses.h>
 
-#define mapHeight 80
+#define mapHeight 25
 #define mapWidth 80
 
 typedef struct SObject{
@@ -18,31 +19,24 @@ void Clearmap(){
         map[0][i] = '.';
     map[0][mapWidth] = '\0';
     for (int j = 1; j < mapHeight; j++)
-        sprintf(map[j], map[0]);
+        sprintf(map[j], "%s", map[0]);
 }
 
 void ShowMap(){
-    map[mapHeight - 1][mapWidth - 1] = '\0';
     for (int j = 0; j < mapHeight; j++)
-        printf(map[j], map[0]);
+        mvprintw(j, 0, "%s", map[j]);
 }
-
 
 void SetObjectPos(TObject *obj, float xPos, float yPos){
-
     (*obj).x = xPos;
     (*obj).y = yPos;
-
 }
-void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHight){
 
-    SetObjectPos(obj, oWidth, oHight);
+void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHight){
+    SetObjectPos(obj, xPos, yPos);
     (*obj).width = oWidth;
     (*obj).hight = oHight;
-
 }
-
-
 
 void PutObjectOnMap(TObject obj){
     int ix = (int)round(obj.x);
@@ -52,17 +46,43 @@ void PutObjectOnMap(TObject obj){
 
     for (int i = ix; i < (ix + iWidth); i++)
         for (int j = iy; j < (iy + iHight); j++)
-            map[j][i] = '0';
-
-
+            if (i >= 0 && i < mapWidth && j >= 0 && j < mapHeight)
+                map[j][i] = '@';
 }
 
 int main(){
+    // Инициализация ncurses
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    curs_set(0);
     
-    InitObject(&mario, 39,10,3,3);
-    Clearmap();
-    PutObjectOnMap(mario);
-    ShowMap();
-
+    InitObject(&mario, 39, 10, 3, 3);
+    
+    int ch;
+    do {
+        Clearmap();
+        PutObjectOnMap(mario);
+        
+        // Управление
+        ch = getch();
+        if (ch != ERR) {
+            if (ch == 'a' || ch == KEY_LEFT) mario.x -= 1;
+            if (ch == 'd' || ch == KEY_RIGHT) mario.x += 1;
+            if (ch == 'w' || ch == KEY_UP) mario.y -= 1;
+            if (ch == 's' || ch == KEY_DOWN) mario.y += 1;
+        }
+        
+        clear();
+        ShowMap();
+        mvprintw(0, 0, "Use WASD/Arrows to move | ESC to exit");
+        refresh();
+        
+        napms(50);
+    } while(ch != 27);
+    
+    endwin();
     return 0;
 }
--- SuperMario/power.sh
+++ SuperMario/power.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
-cd /home/charm/cpp-coroutines/SuperMario
-gcc -o mainCopyWrite mainCopyWrite.c -lm
-./mainCopyWrite
+
+# Цвета для вывода
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}================================${NC}"
+echo -e "${GREEN}   Mario Game Launcher${NC}"
+echo -e "${GREEN}================================${NC}"
+
+
+# Компиляция
+echo -e "${YELLOW}Compiling game...${NC}"
+gcc -o mario_game mainCopyWrite.c -lm -lncurses
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Compilation successful!${NC}"
+    echo -e "${YELLOW}Starting game...${NC}"
+    echo -e "${GREEN}================================${NC}"
+    sleep 1
+    
+    # Запуск игры
+    ./mario_game
+    
+    # Очистка после игры
+    clear
+    echo -e "${GREEN}================================${NC}"
+    echo -e "${GREEN}Game exited!${NC}"
+    echo -e "${GREEN}================================${NC}"
+else
+    echo -e "${RED}Compilation failed!${NC}"
+    exit 1
+fi
